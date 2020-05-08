0510788437cbb13bffa43784996bf2f21fbdc636
Eugene Poroshin <zhenkanet@gmail.com>
2020-05-08 17:55:30 +0300

    init: restart project whith Kotlin

---
+++ app/src/main/java/com/eugene_poroshin/money_manager/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.eugene_poroshin.money_manager
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
