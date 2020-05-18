fc0c9d7f3ef5abfc70baa12bcbb184a628fb0556
Eugene Poroshin <zhenkanet@gmail.com>
2020-05-18 18:26:29 +0300

    refactor: change to Kotlin

---
+++ app/src/main/java/com/eugene_poroshin/money_manager/statistics/ChartColors.kt
@@ -0,0 +1,25 @@
+package com.eugene_poroshin.money_manager.statistics
+
+import android.graphics.Color
+
+object ChartColors {
+    @JvmField
+    val MATERIAL_COLORS = intArrayOf(
+        rgb("#FF5252"),
+        rgb("#FF9800"),
+        rgb("#FFEB3B"),
+        rgb("#8BC34A"),
+        rgb("#03A9F4"),
+        rgb("#536DFE"),
+        rgb("#E040FB"),
+        rgb("#CDDC39")
+    )
+
+    fun rgb(hex: String): Int {
+        val color = hex.replace("#", "").toLong(16).toInt()
+        val r = color shr 16 and 0xFF
+        val g = color shr 8 and 0xFF
+        val b = color shr 0 and 0xFF
+        return Color.rgb(r, g, b)
+    }
+}
